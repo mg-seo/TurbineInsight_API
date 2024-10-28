fa3509b9ffda7442fff8d4b4ee07559b301c6d6c
mugyeong <mgseokr@gmail.com>
2024-10-28 17:03:08 +0900

    businessService 완료

--- src/main/java/com/yinseo/turbineInsightWeb/repository/BusinessRepository.java
+++ src/main/java/com/yinseo/turbineInsightWeb/repository/BusinessRepository.java
@@ -3,6 +3,9 @@ package com.yinseo.turbineInsightWeb.repository;
 import com.yinseo.turbineInsightWeb.entity.Business;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BusinessRepository extends JpaRepository<Business, Long> {
+    List<Business> findByUser_UserId(String userId);
 }
 
--- src/main/java/com/yinseo/turbineInsightWeb/service/BusinessService.java
+++ src/main/java/com/yinseo/turbineInsightWeb/service/BusinessService.java
@@ -5,5 +5,9 @@ import com.yinseo.turbineInsightWeb.entity.Business;
 import java.util.List;
 
 public interface BusinessService {
-    List<Business> getBusinessById(String userId);
+    Business createBusiness(String businessName, String userId); // 사업체 이름과 userId로 추가
+    List<Business> getBusinessesByUserId(String userId); // userId로 사업체 목록 조회
+    Business updateBusinessName(Long businessId, String businessName); // 사업체 이름만 업데이트
+    Business updateMemo(Long businessId, String memo);           // memo 컬럼만 업데이트
+    void deleteBusiness(Long businessId);                        // 특정 ID의 사업체 삭제
 }
