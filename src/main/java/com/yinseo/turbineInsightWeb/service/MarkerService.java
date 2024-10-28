bd50af2305904d92d98072a0a5ee7376755da077
jinseok1212 <dlwlstjr110@naver.com>
2024-10-28 17:06:22 +0900

    마커 기초 서비스 구현

--- src/main/java/com/yinseo/turbineInsightWeb/service/MarkerService.java
+++ src/main/java/com/yinseo/turbineInsightWeb/service/MarkerService.java
@@ -1,4 +1,18 @@
 package com.yinseo.turbineInsightWeb.service;
 
+import com.yinseo.turbineInsightWeb.entity.Marker;
+
+import java.util.List;
+
 public interface MarkerService {
+
+    //사업아이디로 마커 불러오기
+    List<Marker> getMarkersById(Long business_id);
+
+    //저장
+    Marker saveMarker(Marker marker);
+
+    void deleteMarkerById(Long marker_id);
+
+
 }
