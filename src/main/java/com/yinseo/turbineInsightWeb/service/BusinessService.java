66a6f923562ea15675394f0eebb4fe559c957538
mugyeong <mgseokr@gmail.com>
2024-10-28 15:29:43 +0900

    businesses 테이블에 memo 컬럼 추가

--- src/main/java/com/yinseo/turbineInsightWeb/service/BusinessService.java
+++ src/main/java/com/yinseo/turbineInsightWeb/service/BusinessService.java
@@ -1,4 +1,9 @@
 package com.yinseo.turbineInsightWeb.service;
 
+import com.yinseo.turbineInsightWeb.entity.Business;
+
+import java.util.List;
+
 public interface BusinessService {
+    List<Business> getBusinessById(String userId);
 }
--- src/main/java/com/yinseo/turbineInsightWeb/service/BusinessServiceImpl.java
+++ src/main/java/com/yinseo/turbineInsightWeb/service/BusinessServiceImpl.java
@@ -1,4 +1,12 @@
 package com.yinseo.turbineInsightWeb.service;
 
+import com.yinseo.turbineInsightWeb.entity.Business;
+
+import java.util.List;
+
 public class BusinessServiceImpl implements BusinessService {
+    @Override
+    public List<Business> getBusinessById(String userId) {
+        return List.of();
+    }
 }
